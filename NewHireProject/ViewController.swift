c6cae8eb611302063e5747d026f1a9b1b0b83545
David Godon <dgodn23@gmail.com>
2020-06-03 10:26:17 -0400

    first commit

---
+++ NewHireProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewHireProject
+//
+//  Created by David Godon on 6/3/20.
+//  Copyright © 2020 HWKR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
